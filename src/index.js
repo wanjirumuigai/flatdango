ae60adc566c0e2463ec24a7dc894b9908ecca66d
Charles Swaleh <charlswall@live.com>
2023-01-02 22:45:23 +0300

    fix bug on sold out function

--- src/index.js
+++ src/index.js
@@ -1,34 +1,44 @@
 // Your code here
 let url = "http://localhost:3000/films"
-let ticket;
 
-fetch(url)
-.then(res => res.json())
-.then(data => {
+document.addEventListener("DOMContentLoaded", () => {
+  loadMovies()
+})
+function loadMovies(){
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
     // data.forEach(element => {console.log(element.title)
     
-    displayFirstMovie(data[0])
     renderMovies(data)
+    displayFirstMovie(data[0])
     
-})
+  })
+}
 
 
 function displayFirstMovie(movieObj) {
-    
-                
-        
-        document.querySelector('#poster').src = movieObj.poster
-        document.querySelector('#poster').alt = movieObj.title
-        document.querySelector('#title').innerHTML = movieObj.title
-        document.querySelector('#runtime').innerHTML = `${movieObj.runtime} minutes`
-        document.querySelector('#film-info').innerHTML = movieObj.description
-        document.querySelector('#showtime').innerHTML = movieObj.showtime
-        document.querySelector('#title').dataset.id = movieObj.id
-        
-        document.querySelector('#ticket-num').textContent = movieObj.capacity - movieObj.tickets_sold
-        
-        
-    }
+  if ((movieObj.capacity - movieObj.tickets_sold) === 0){
+    document.getElementById("buy-ticket").textContent = "Sold Out"
+    document.getElementById("buy-ticket").disabled = true
+    document.getElementById("buy-ticket").classList.add("sold-out")
+  }
+  else {
+    document.getElementById("buy-ticket").textContent = "Buy Ticket"
+    document.getElementById("buy-ticket").disabled = false
+    document.getElementById("buy-ticket").classList.remove("sold-out") 
+  }
+  document.querySelector('#poster').src = movieObj.poster
+  document.querySelector('#poster').alt = movieObj.title
+  document.querySelector('#title').innerHTML = movieObj.title
+  document.getElementById("title").dataset.capacity = movieObj.capacity
+  document.getElementById("title").dataset.sold = movieObj.tickets_sold
+  document.querySelector('#runtime').innerHTML = `${movieObj.runtime} minutes`
+  document.querySelector('#film-info').innerHTML = movieObj.description
+  document.querySelector('#showtime').innerHTML = movieObj.showtime
+  document.querySelector('#title').dataset.id = movieObj.id
+  document.querySelector('#ticket-num').textContent = movieObj.capacity - movieObj.tickets_sold
+}
 
 document.getElementById('buy-ticket').addEventListener('click', sellTicket)
    
@@ -38,7 +48,6 @@ function renderMovies(movies) {
     ul.innerHTML = ''
 
     movies.forEach(item => {
-       
         const movieTitle = item.title 
         let btn = document.createElement('button')
         btn.addEventListener('click', (e) => {
@@ -46,6 +55,9 @@ function renderMovies(movies) {
             deleteMovie (item.id)})
         btn.textContent = 'x'
         let li = document.createElement('li')
+        if (item.capacity === item.tickets_sold){
+          li.classList.add("sold-out")
+        }
         li.textContent = `${movieTitle.toUpperCase()} `
         li.dataset.id = item.id
         li.classList.add('filmList') 
@@ -58,16 +70,30 @@ function renderMovies(movies) {
     })
 }
 
-function sellTicket(movieObj) {
-   
-  console.log(document.getElementById('title').dataset.id)
+function loadMovieByID(id){
+  fetch(`${url}/${id}`)
+  .then(res => res.json())
+  .then(data => {
+    // data.forEach(element => {console.log(element.title)
+    displayFirstMovie(data)
     
-    
-        
-    fetch(`${url}/${movieObj.id}`,{
+  })
+}
+
+function sellTicket() {
+  const filmID = document.getElementById('title').dataset.id
+  const capacity = parseInt(document.getElementById("title").dataset.capacity)
+  let ticketsSold = parseInt(document.getElementById("title").dataset.sold)
+  let remainingTickets = capacity - ticketsSold
+  remainingTickets -= 1
+  document.getElementById("ticket-num").textContent = remainingTickets
+  document.getElementById("title").dataset.sold = ticketsSold + 1
+
+    fetch(`${url}/${filmID}`,{
         method: 'PATCH',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
         },
         body: JSON.stringify({
             tickets_sold: ticketsSold + 1
@@ -75,7 +101,6 @@ function sellTicket(movieObj) {
     })
     .then(res => res.json())
     .then(movieObj => console.log(movieObj))
-    
 }
 function deleteMovie(movieID) {
     
